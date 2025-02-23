59e799b1970591642e12aa7fc197fbe62b68074e
mikimena01 <mikimena01@gmail.com>
2025-02-23 17:49:58 +0100

    added db

--- menTHOR/settings.py
+++ menTHOR/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*1ia_x)%o&)d1%#z7i!_-zrvtdc%&92o%g4!f*_%+(=)t+pok5'
 #SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = os.environ.get('DEBUG', '0').lower() in ['true', 't', '1']
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split()
