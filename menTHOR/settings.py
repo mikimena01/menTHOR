6df82fb8aea13c2bb8d60836f91869bb06e3ee2b
mikimena01 <mikimena01@gmail.com>
2025-02-23 11:03:44 +0100

    added db

--- menTHOR/settings.py
+++ menTHOR/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'menTHOR.wsgi.application'
 
 import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 wxa_api_key = os.getenv("WXA_API_KEY", "default")
